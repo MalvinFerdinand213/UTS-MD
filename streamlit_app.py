2dfc722477e70329b7f3eb46b92f0b9c778dda4d
MalvinFerdinand213 <74392043+MalvinFerdinand213@users.noreply.github.com>
2025-04-14 22:29:25 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
 
 st.title('🎈 App Name')
 
