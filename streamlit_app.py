4b6f4930f4c9c250e9a9900e333d7b5ec838e8df
MalvinFerdinand213 <74392043+MalvinFerdinand213@users.noreply.github.com>
2025-04-20 00:19:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import joblib
 import numpy as np
+import pandas as pd
 
 # Load model pipeline (model dan preprocessing dalam satu kesatuan)
 pipeline = joblib.load('trained_model.pkl')
