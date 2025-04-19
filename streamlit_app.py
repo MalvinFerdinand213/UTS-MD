62ae4e76b6baed7780f227f2bd8dd804c18a25ec
MalvinFerdinand213 <74392043+MalvinFerdinand213@users.noreply.github.com>
2025-04-19 23:43:40 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,18 +10,9 @@ user_input = [erythema, scaling, definite_borders, itching, koebner_phenomenon, 
                 vacuolisation_damage_basal_layer, spongiosis, saw_tooth_appearance_retes, follicular_horn_plug, perifollicular_parakeratosis,\
                 inflammatory_mononuclear_infiltrate, band_like_infiltrate, age]
 
-  model_filename = 'trained_model.pkl'
-  model = load_model(model_filename)
-  prediction = predict_with_model(model, user_input)
-  st.write('The prediction output is: ', prediction)
+model_filename = 'trained_model.pkl'
+model = load_model(model_filename)
+prediction = predict_with_model(model, user_input)
+st.write('The prediction output is: ', prediction)
 features = np.array([[hotel_encoded, lead_time, adults, children,
                       weekend_nights, week_nights, previous_cancellations]])
-
-# Tombol prediksi
-if st.button("Prediksi"):
-    prediction = model.predict(features)
-
-    if prediction[0] == 1:
-        st.error("Booking kemungkinan akan DIBATALKAN")
-    else:
-        st.success("Booking kemungkinan TIDAK DIBATALKAN")
