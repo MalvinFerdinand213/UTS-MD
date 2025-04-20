ff5af3bd9efa7de0a3f9bd68fd8d5ef011a330c3
MalvinFerdinand213 <74392043+MalvinFerdinand213@users.noreply.github.com>
2025-04-20 09:54:33 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,14 +12,11 @@ no_of_adults = st.number_input("Jumlah Dewasa", min_value=0, value=2)
 no_of_children = st.number_input("Jumlah Anak", min_value=0, value=0)
 no_of_weekend_nights = st.number_input("Jumlah Malam Akhir Pekan", min_value=0)
 no_of_week_nights = st.number_input("Jumlah Malam Hari Kerja", min_value=0)
-#type_of_meal_plan = st.selectbox("Paket Makanan", ['Meal Plan 1', 'Meal Plan 2', 'Meal Plan 3'])
-required_car_parking_space = st.selectbox("Perlu Parkir Mobil?", [0, 1])
-#room_type_reserved = st.selectbox("Tipe Kamar", ['Room_Type 1', 'Room_Type 2', 'Room_Type 3', 'Room_Type 4', 'Room_Type 5', 'Room_Type 6', 'Room_Type 7'])
+required_car_parking_space = st.selectbox("Perlu Parkir Mobil?", [0, 1]
 lead_time = st.number_input("Lead Time (hari sebelum check-in)", min_value=0)
 arrival_year = st.selectbox("Tahun Kedatangan", [2017, 2018])
 arrival_month = st.selectbox("Bulan Kedatangan", list(range(1, 13)))
 arrival_date = st.selectbox("Tanggal Kedatangan", list(range(1, 32)))
-#market_segment_type = st.selectbox("Segment Pasar", ['Offline', 'Online', 'Corporate', 'Complementary', 'Aviation'])
 repeated_guest = st.selectbox("Tamu Berulang?", [0, 1])
 no_of_previous_cancellations = st.number_input("Jumlah Pembatalan Sebelumnya", min_value=0)
 no_of_previous_bookings_not_canceled = st.number_input("Jumlah Booking Lalu Tidak Dibatalkan", min_value=0)
@@ -32,14 +29,11 @@ user_input = pd.DataFrame({
     'no_of_children': [no_of_children],
     'no_of_weekend_nights': [no_of_weekend_nights],
     'no_of_week_nights': [no_of_week_nights],
-    #'type_of_meal_plan': [type_of_meal_plan],
     'required_car_parking_space': [required_car_parking_space],
-    #'room_type_reserved': [room_type_reserved],
     'lead_time': [lead_time],
     'arrival_year': [arrival_year],
     'arrival_month': [arrival_month],
     'arrival_date': [arrival_date],
-    #'market_segment_type': [market_segment_type],
     'repeated_guest': [repeated_guest],
     'no_of_previous_cancellations': [no_of_previous_cancellations],
     'no_of_previous_bookings_not_canceled': [no_of_previous_bookings_not_canceled],
